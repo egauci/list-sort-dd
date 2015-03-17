71361560d5782adfb5b797e3aad0b14e31340599
unknown <egauci@DTC2100CC489BDA.ent.wfb.bank.corp>
2015-03-17 16:06:24 -0700

    Fixes

--- lib/components/dds.jsx
+++ lib/components/dds.jsx
@@ -4,18 +4,32 @@ import React from 'react';
 
 let DDS = React.createClass({
   propTypes: {
-    list: React.PropTypes.array.esRequired
+    list: React.PropTypes.array.isRequired
+  },
+  onDragStart(e) {
+    debugger;
   },
   render() {
     return (
       <div className="dds">
         <ul>
-          this.props.list.map(itm => {
-            return (
-              <li key={itm.id}
-              >itm.label</li>
-            );
-          });
+          {
+            this.props.list.map(itm => {
+              return (
+                <li
+                  key={itm.id}
+                  data-id={itm.id}
+                  draggable="true"
+                  tabIndex="0"
+                  aria-grabbed={itm.dragging ? "true" : "false"}
+                  aria-dropeffect="none"
+                  onDragStart={this.onDragStart}
+                >
+                  {itm.label}
+                </li>
+              );
+            })
+          }
         </ul>
       </div>
     );
