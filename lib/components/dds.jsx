5beb1cd843484b48994018e689763ce317b64f3c
unknown <egauci@DTC2100CC489BDA.ent.wfb.bank.corp>
2015-03-18 15:52:13 -0700

    Basic mouse-based d/d

--- lib/components/dds.jsx
+++ lib/components/dds.jsx
@@ -4,26 +4,85 @@ import React from 'react';
 
 let DDS = React.createClass({
   propTypes: {
-    list: React.PropTypes.array.isRequired
+    config: React.PropTypes.shape({meta: React.PropTypes.object.isRequired,
+                                   list: React.PropTypes.array.isRequired}).isRequired,
+    callBack: React.PropTypes.func.isRequired
   },
   onDragStart(e) {
-    debugger;
+    let id = e.currentTarget.getAttribute('data-id'),
+        pos = e.currentTarget.getAttribute('data-pos');
+    console.log('onDragStart: ' + id + ', pos: ' + pos);
+    e.dataTransfer.setData('text', '');
+    this.props.callBack({
+      action: 'onDragStart',
+      id: id,
+      pos: parseInt(pos, 10)
+    });
+  },
+  onDragOver(e) {
+    let id = e.currentTarget.getAttribute('data-id'),
+        pos = e.currentTarget.getAttribute('data-pos');
+    //console.log('onDragOver: ' + id + ', pos: ' + pos);
+    if (this.props.config.meta.dragging !== id) {
+      if (this.props.config.meta.dragging) {
+        e.preventDefault();
+      }
+      if (id !== this.props.config.meta.over) {
+        console.log('Calling onDragOver callback: ' + id);
+        this.props.callBack({
+          action: 'onDragOver',
+          id: id,
+          pos: parseInt(pos, 10)
+        });
+      }
+    }
+  },
+  onDrop(e) {
+    console.log('onDrop');
+    this.props.callBack({
+      action: 'onDrop'
+    });
+  },
+  onDragEnd(e) {
+    console.log('onDragEnd');
+    this.props.callBack({
+      action: 'onDragEnd'
+    });
+  },
+  onDragLeave(e) {
+    console.log('onDragLeave');
+    this.props.callBack({
+      action: 'onDragLeave'
+    });
   },
   render() {
     return (
       <div className="dds">
         <ul>
           {
-            this.props.list.map(itm => {
+            this.props.config.list.map((itm, ix) => {
               return (
                 <li
                   key={itm.id}
                   data-id={itm.id}
+                  data-pos={String(ix)}
                   draggable="true"
-                  tabIndex="0"
-                  aria-grabbed={itm.dragging ? "true" : "false"}
-                  aria-dropeffect="none"
+                  tabIndex={this.props.config.meta.dragging !== itm.id ? "0" : null}
+                  aria-grabbed= {(this.props.config.meta.dragging &&
+                                 this.props.config.meta.dragging === itm.id) ?
+                                 "true" : "false"}
+                  aria-dropeffect={(this.props.config.meta.dragging &&
+                                 this.props.config.meta.dragging !== itm.id) ?
+                                 "move" : "none"}
+                  className={(this.props.config.meta.over &&
+                              this.props.config.meta.over === itm.id &&
+                              this.props.config.meta.dragging !== itm.id) ?
+                              "over" : ""}
                   onDragStart={this.onDragStart}
+                  onDragOver={this.onDragOver}
+                  onDrop={this.onDrop}
+                  onDragEnd={this.onDragEnd}
+                  onDragLeave={this.onDragLeave}
                 >
                   {itm.label}
                 </li>
--- lib/main.jsx
+++ lib/main.jsx
@@ -4,6 +4,41 @@ import React from 'react';
 import DDS from './components/dds.jsx!';
 import config from './dconfig';
 
-var dds;
+let dds;
 
-dds = React.render(<DDS list={config.data.list} />, document.querySelector('#ddCtr'));
+let callBack = function(dta) {
+  switch(dta.action) {
+    case 'onDragStart':
+      config.meta.dragging = dta.id;
+      config.meta.draggingPos = dta.pos;
+      dds.setProps({config: config});
+      //console.log('Main onDragStart: ' + JSON.stringify(config.meta, null, '  '));
+      break;
+    case 'onDragOver':
+      config.meta.over = dta.id;
+      config.meta.overPos = dta.pos;
+      dds.setProps({config: config});
+      //console.log('Main onDragOver: ' + JSON.stringify(config.meta, null, '  '));
+      break;
+    case 'onDrop':
+      //console.log('Main onDrop: ' + JSON.stringify(config.meta, null, '  '));
+      let moved = config.list.splice(config.meta.draggingPos, 1);
+      config.list.splice(config.meta.overPos, 0, moved[0]);
+      //console.log('setting to: ' + JSON.stringify(config, null, '  '));
+      dds.setProps({config: config});
+      break;
+    case 'onDragLeave':
+      console.log('Main onDragLeave');
+      config.meta.over = config.meta.overPos = null;
+      dds.setProps({config: config});
+      break;
+    case 'onDragEnd':
+      config.meta.dragging = config.meta.draggingPos =
+        config.meta.over = config.meta.overPos = null;
+      dds.setProps({config: config});
+      //console.log('onDragEnd: ' + JSON.stringify(config.meta, null, '  '));
+      break;
+  }
+};
+
+dds = React.render(<DDS config={config} callBack={callBack} />, document.querySelector('#ddCtr'));
